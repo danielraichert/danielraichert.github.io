1fadb24b758ddaa6ed4c709beb08e051c00e7581
danielraichert <danraichert@gmail.com>
2024-11-01 12:25:33 +0200

    Update token

--- gentoken.js
+++ gentoken.js
@@ -1,13 +1,13 @@
 var jwt = require("jsonwebtoken");
 
-private_key='-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIMN6kbeP011DGZ/N0mbEToX2vFSlw4cf+yK8DTO7VGOSoAoGCCqGSM49\nAwEHoUQDQgAEQMQcwI7nmmWmBRdrm+9geMDk9AFh0yiXINhDYl9Zhy4GbGLeinMa\niDLY6aQZLlQxk5vSG0WVB6NvA3mQAAGW0w==\n-----END EC PRIVATE KEY-----\n';
+private_key='-----BEGIN EC PRIVATE KEY-----\nMHcCAQEEIOiTxhv2MTDNmUg/m3PhbVaDiwIyo+dTlH7/KVaAKK3BoAoGCCqGSM49\nAwEHoUQDQgAEwTNEhyZ22JnNlfnuq3LmKITEwqfJ0RylCYmT2zYYv1Y7u32/4rjy\nVKEsRaTU5IymbLbsjr09NUTEU9wtrG6f8Q==\n-----END EC PRIVATE KEY-----';
 
 claims = {
     'name': 'Daniel Raichert',
     'given_name': 'Daniel',
     'family_name': 'Raichert',
     'email': 'daniel.raichert@glia.com',
-    'sub': 'daniel.raichert',
+    'sub': 'john.doe',
     'exp': Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 5,
     'iat': Math.floor(Date.now() / 1000)
 }
